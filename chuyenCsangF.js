ef9d02b93f4999e87ea05807ed9bc0705c46c4ab
bachngocng <nnbach97@gmail.com>
2021-12-09 11:21:50 +0700

    da them 4 file moi

---
+++ chuyenCsangF.js
@@ -0,0 +1,4 @@
+
+let c=parseFloat(prompt('Enter the Celcius'))
+let f= (c*9)/5+32;
+ f=alert('Độ F là:' +f)
---
+++ dientichhinhtron.js
@@ -0,0 +1,6 @@
+
+let r = parseFloat(prompt('Nhập bán kính'));
+const pi=3.14
+let area = r*r*pi;
+area=alert('Diện tích hình tròn là:'+ area)
+
